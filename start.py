53941f1fc2b5ab5b24359e13203fd2b273f84054
KhinYadanaThein <angelchit35@gmail.com>
2018-06-26 13:39:29 +0530

    added hello

--- start.py
+++ start.py
@@ -3,10 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def start():
     return "Hello World"
 
-@app.route("/tiide")
-def tiide():
-    return "Welcome to TIIDE World":wq
 
